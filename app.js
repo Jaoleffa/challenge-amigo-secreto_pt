0277f096c738f078b165442cf8f87448e48fd85d
João Leffa <joaoleapolinario@gmail.com>
2025-01-21 21:20:50 -0300

    função de adicionar amigos na lista

--- app.js
+++ app.js
@@ -3,14 +3,30 @@ let tituloH2 = document.querySelector('h2')
 
 function adicionarAmigo() {
     tituloH2.innerHTML = 'Digite o nome dos seus amigos';
-    if (document.getElementById('amigo').value != '') {
-        let nome = document.getElementById('amigo').value;
+    let nome = document.getElementById('amigo').value;
+    let tabelaDeAmigos = document.getElementById('listaAmigos');
+    tabelaDeAmigos.innerHTML = '';
+
+    if (nome !== '' && !listaDeAmigos.includes(nome)) {
+
         listaDeAmigos.push(nome);
         console.log(listaDeAmigos);
+        tituloH2.innerHTML = 'Amigo adicionado!';
         limparCampo();
+
+    } else if (nome == '') {
+        tituloH2.innerHTML = "Digite um nome válido";
     } else {
-        tituloH2.innerHTML = 'Digite um nome válido';
+        tituloH2.innerHTML = "Nome já adicionado";
     }
+
+    listaDeAmigos.forEach (amigo => {
+        let amigoNaLista = document.createElement('li');
+        amigoNaLista.textContent = amigo;
+        tabelaDeAmigos.appendChild(amigoNaLista);
+    }
+  )
+
 }
 
 function limparCampo() {
